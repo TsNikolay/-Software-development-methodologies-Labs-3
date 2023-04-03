264304863f5a37894eb38e192fde52fff9b56d36
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-04-04 00:13:47 +0200

    Returned removed response to api.py

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -3,6 +3,16 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get('')
+def hello_world() -> dict:
+    return {
+        'Fullname': 'Tsaryk Mykola',
+        'Sex': 'Male',
+        'Nationality': 'Ukr',
+        'DocumentID': '12345678',
+        'Speciality': 'Web-developer',
+        'Languages': 'JavaScript, HTML, CSS'
+    }
 
 @router.get('/matrix')
 def matrix() -> dict:
