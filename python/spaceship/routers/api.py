6d2a15c73f6cff428ee698c10857db1d8ac131c1
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-04-03 23:43:00 +0200

    Changed base image to alpine. Using numpy.
    In this commit, I added some dependencies, rewrote api.py, changed the base image to compare against slim-bullseye

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,15 +1,20 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get('')
-def hello_world() -> dict:
-    return {
-        'Fullname': 'Tsaryk Mykola',
-        'Sex': 'Male',
-        'Nationality': 'Ukr',
-        'DocumentID': '12345678',
-        'Speciality': 'Web-developer',
-        'Languages': 'JavaScript, HTML, CSS'
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    product = np.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
     }
+
+    return result
